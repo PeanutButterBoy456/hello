11a940c825ecbebad3cea6fd775fb9ba2e6b6493
Ch3ddar77994 <44814298+Ch3ddar77994@users.noreply.github.com>
2018-11-13 10:44:47 -0700

    Create old_code.c

---
+++ old_code.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+
+//Salt output is 2/3 times input
+//H2O output is 11/12 times input
+
+float filter(float h2o_in, float nacl_in, float  fiter_count, float time);
+
+int main(void){           
+
+float water_input = 0.0; // In Liters
+float salt_input = 0.0; // In PSU
+float num_filters = 0.0; // As pregnancy rate
+float water_output = 0.0; // In Liters
+float salt_output = 0.0; // In PSU
+float time_taken=0.0; // In Light Years
+float  dionization_cost
+
+printf("Mangrove Madness \n Salt Water Filtration System \n\n);
+
+printf("Please enter the amount of water that is going into the system in liters:\t")
+scanf("%f", &water_input);
+printf("Please enter the salinity of the water in PSU:\t");
+scanf("%f", &salt_input);
+printf("Please enter the amount of filters: \t");
+scanf("%f",&num_filters);
+printf("Calculating. This may take a minute");
+sleep(1);
+printf(".");
+sleep(1);
+printf(".");
+sleep(1);
+printf(".");
+sleep(1);
+printf(".");
+sleep(1);
+printf(".");
+printf(".");
+sleep(1);
+printf(".");
+
+water_output= water_input * (11/12);
+
+
+
+
+return 0;
+
+}
+
+float filter(float h2o_in, float nacl_concentrate, float  fiter_count, float time, cost){
+return ((h2o_in / (nacl_concentrate / 1000)) * filter_count) * time
+}
+
+
+
+/* Here is some interesting information:  The distillation process can take a relatively long time, especially when large amounts of water need to be boiled, cooled, and collected. In addition, this process requires fuel to heat the water and a sterile container to store it in. When distilled water is exposed to the air over time, it essentially becomes deionized water.
+Deionization, on the other hand, can be performed relatively quickly – especially if a mixed bed resin is used, so the water only needs to pass through one time. Many systems use two mixed bed cartridges or tanks, helping to ensure that all ions have been removed, but it's still a relatively fast process when compared to distillation. In addition, deionization is a chemical process, so energy is only typically needed to monitor the process and move the water through the system. If the DI resin is regenerated on site, this can add both time and expense to the process, however.
+
+
+return 0;
+
+}
+
+float filter(float h2o_in, float nacl_concentrate, float  fiter_count, float time, cost){
+return ((h2o_in / (nacl_concentrate / 1000)) * filter_count) * time
+}
+*/
+
