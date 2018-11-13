3e5a0018f4c66aca5649dcc6d1c94c684e392eba
PeanutButterBoy456 <45012137+PeanutButterBoy456@users.noreply.github.com>
2018-11-13 10:56:54 -0700

    Update and rename main.h to main.c

---
+++ main.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main(void){          
+  
+float water_input = 0.0; // In Liters
+float salt_input = 0.0; // In PSU
+float num_filters = 0.0; // As pregnancy rate
+float water_output = 0.0; // In Liters
+float salt_output = 0.0; // In PSU
+float time_taken=0.0; // In Light Years
+
+
+printf("Mangrove Madness \n Salt Water Filtration System \n\n");
+printf("Please enter the amount of water that is going into the system in liters:\t");
+scanf("%f", &water_input);
+printf("Please enter the salinity of the water in PSU:\t");
+scanf("%f", &salt_input);
+printf("Please enter the amount of filters: \t");
+scanf("%f",&num_filters);
+printf("Calculating. This may take a minute\n");
+
+  printf("welcome m
+
+return 0;
+}
